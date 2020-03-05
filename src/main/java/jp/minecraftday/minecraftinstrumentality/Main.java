d33978a178c22b57b45f5b4518c5a012d6bdef66
yokmama <hijirinhijirin@gmail.com>
2020-03-05 21:54:01 +0900

    チャットの名前を　役職　ニックネームにし、ニックネームにマウスをあてると名前がホバーする機能を追加 ローマ字を自動変換するhiraganaコマンドを追加

--- src/main/java/jp/minecraftday/minecraftinstrumentality/Main.java
+++ src/main/java/jp/minecraftday/minecraftinstrumentality/Main.java
@@ -1,26 +1,52 @@
 package jp.minecraftday.minecraftinstrumentality;
 
-import jp.minecraftday.minecraftinstrumentality.command.GameCommandExecutor;
-import jp.minecraftday.minecraftinstrumentality.command.MainCommandExecutor;
-import jp.minecraftday.minecraftinstrumentality.command.SetItemNameCommandExecutor;
-import jp.minecraftday.minecraftinstrumentality.command.VoteCommandExecutor;
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.IEssentials;
+import com.earth2me.essentials.User;
+import com.earth2me.essentials.perm.PermissionsHandler;
+import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.dependencies.jda.core.entities.PermissionOverride;
+import jp.minecraftday.minecraftinstrumentality.command.*;
+import jp.minecraftday.minecraftinstrumentality.utils.Configuration;
+import jp.minecraftday.minecraftinstrumentality.utils.KanaConverter;
+import jp.minecraftday.minecraftinstrumentality.utils.TellRawGenerator;
+import jp.minecraftday.minecraftinstrumentality.utils.UserConfiguration;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginLogger;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Main extends JavaPlugin implements Listener {
     GameCommandExecutor gameCommandExecutor;
+    UserConfiguration userConfiguration;
+
+    private DiscordSRV discordSRV = null;
+    private Essentials essentials = null;
 
     @Override
     public void onEnable() {
         super.onEnable();
 
         gameCommandExecutor = new GameCommandExecutor(this);
+
+        userConfiguration = new UserConfiguration(getDataFolder());
+
+        Essentials ess = getPlugin(Essentials.class);
+        if (ess!=null && !ess.isEnabled()) {
+            essentials = ess;
+        }
+
+        discordSRV = DiscordSRV.getPlugin();
 
         getServer().getPluginManager().registerEvents(this, this);
         getCommand("md").setExecutor(new MainCommandExecutor(this));
@@ -30,8 +56,13 @@ public final class Main extends JavaPlugin implements Listener {
         getCommand("vote").setExecutor(vote);
         getCommand("voteyes").setExecutor(vote);
         getCommand("voteno").setExecutor(vote);
+        getCommand("hiragana").setExecutor(new TranslateCommandExecutor(this));
 
         saveDefaultConfig();
+    }
+
+    public Configuration getUserConfiguration(Player player){
+        return userConfiguration.getUserConfig(player);
     }
 
     @Override
@@ -60,4 +91,44 @@ public final class Main extends JavaPlugin implements Listener {
             }
         });
     }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChat(AsyncPlayerChatEvent event) {
+        event.setCancelled(true);
+
+        String fixString = event.getMessage();
+        Configuration configuration = getUserConfiguration(event.getPlayer());
+        boolean hiragana = configuration.getBoolean("hiragana");
+        if(hiragana) {
+            fixString = KanaConverter.conv(fixString);
+        }
+
+        PermissionsHandler handler = essentials.getPermissionsHandler();
+
+        //Gm yokmama »
+        TellRawGenerator builder = new TellRawGenerator();
+        builder.append(handler.getPrefix(event.getPlayer())+" ", true);
+        builder.append(event.getPlayer().getDisplayName(), true, event.getPlayer().getName());
+        builder.append("&f>");
+        builder.append(fixString);
+
+        String msg = builder.toString();
+        event.getPlayer().getWorld().getPlayers().forEach(p->{
+            StringBuilder buf = new StringBuilder();
+            buf.append("tellraw ").append(p.getName()).append(" ").append(msg);
+
+            Threading.postToServerThread(new Threading.Task(this) {
+                @Override
+                public void execute() {
+                    p.performCommand(buf.toString());
+                }
+            });
+
+        });
+
+        if(discordSRV!=null) {
+            discordSRV.processChatMessage(event.getPlayer(), fixString, DiscordSRV.getPlugin().getChannels().size() == 1 ? null : "global", false);
+        }
+    }
+
 }
---
+++ src/main/java/jp/minecraftday/minecraftinstrumentality/utils/UserConfiguration.java
@@ -0,0 +1,31 @@
+package jp.minecraftday.minecraftinstrumentality.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+public class UserConfiguration {
+    final File userFolder;
+    final Map<String, Configuration> map = new HashMap<>();
+
+    public UserConfiguration(File folder){
+        userFolder = new File(folder, "userdata");
+        if(!userFolder.exists()){
+            userFolder.mkdirs();
+        }
+    }
+
+    public Configuration getUserConfig(final Player player) {
+        String uuid = player.getUniqueId().toString();
+        if(map.containsKey(uuid)) return map.get(uuid);
+
+        Configuration conf = new Configuration(new File(userFolder, uuid+".yml"));
+        map.put(uuid, conf);
+        return conf;
+    }
+}
